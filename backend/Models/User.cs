033419b1f905dcba8a74cf42532140905db39c6e
valiangspe <valian@gspe.co.id>
2026-03-04 09:57:50 +0700

    lastlogin

--- backend/Models/User.cs
+++ backend/Models/User.cs
@@ -6,6 +6,6 @@ public class User
     public required string Name { get; set; }
     public required string Email { get; set; }
     public required string Role { get; set; }
-    public required string LastLogin { get; set; }
+    public string? LastLogin { get; set; }
     public required string Status { get; set; }
 }
